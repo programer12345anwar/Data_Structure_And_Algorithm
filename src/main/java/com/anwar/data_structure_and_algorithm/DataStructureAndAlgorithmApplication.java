051012c5cdd149dd43ef4eee8667d0b2666b0648
Md Anwar Alam <mdanwar40212@gmail.com>
2025-12-01 10:02:50 +0530

    initial commit of data structure and algorithm

---
+++ src/main/java/com/anwar/data_structure_and_algorithm/DataStructureAndAlgorithmApplication.java
@@ -0,0 +1,14 @@
+package com.anwar.data_structure_and_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAndAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAndAlgorithmApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/com/anwar/data_structure_and_algorithm/DataStructureAndAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anwar.data_structure_and_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureAndAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
