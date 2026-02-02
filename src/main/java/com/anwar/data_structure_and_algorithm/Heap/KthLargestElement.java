50afd5a86bdfd5c6faaf86a85ee002940d667dc3
Md Anwar Alam <mdanwar40212@gmail.com>
2026-02-02 13:07:43 +0530

    kth largest element

---
+++ src/main/java/com/anwar/data_structure_and_algorithm/Heap/KthLargestElement.java
@@ -0,0 +1,12 @@
+package com.anwar.data_structure_and_algorithm.Heap;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    int kthLargest(int[] arr, int n){
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+    }
+    public static void main(String[] args) {
+
+    }
+}
